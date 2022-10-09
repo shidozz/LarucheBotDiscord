d764ade162d021a2eb0d51e5a04711ec7e125302
shidozz <iroxmmmmo@gmail.com>
2022-10-09 14:36:24 +0200

    V1

--- buttons/welcome/refuse-button.js
+++ buttons/welcome/refuse-button.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'refuse-button',
     async runInteraction(client, interaction) {
         try {
-            await interaction.member.send(`Tu n'a pas accepté les règles, du coup je t'ai kick !!`)
+            await interaction.member.send(`Tu n'as pas accepté les règles. Tu est libre de revenir si tu accepte les règles !`)
         } catch(e) {
-            await interaction.reply(`Le membre ${interaction.member.displayName} n'a pas accepté les règles, je l'ai kick!!`)
+            await interaction.reply(`Le membre ${interaction.member.displayName} n'a pas accepté les règles, il a donc était kick du serveur !`)
         }
 
-        await interaction.member.kick('Il n\'a pas accepté les règles !!');
+        await interaction.member.kick('Il n\'a pas accepté les règles !');
     }
 };
--- commands/admin/welcome.js
+++ commands/admin/welcome.js
@@ -19,15 +19,15 @@ const buttons = new MessageActionRow()
 const welcomeEmbed = new MessageEmbed()
     .setColor('#0099ff')
     .setTitle(`**Réglement du serveur !**`)
-    .setDescription(`Bienvenue parmis nous,\n\n Afin de visualiser les divers salons, merci de lire et validé le règlement.`)
+    .setDescription(`Bienvenue parmis nous,\n\n Afin de visualiser les divers salons, merci de lire et validé le règlement.\n\n`)
     .addFields(
-        { name: '**REGLE N° 1 :**', value: `Le respect et la rigueur sont  obligatoires` },
-        { name: '**REGLE N° 2 :**', value: `Les propos racistes, homophobes ou discriminations qui auraient pour but d'insulter ou blesser quelqu'un,\n entraineront un ban définitif de ce serveur.` },
-        { name: '**REGLE N° 3 :**', value: `Les publicités et spams sont interdits, pour tout partenariat il vous faudra ouvrir un ticket pour ainsi\n en parler avec l'administration.` },
-        { name: '**REGLE N° 4 :**', value: `Toute forme de harcèlement entrainera un ban définitif.` },
-        { name: '**REGLE N° 5 :**', value: `Toutes identifications inutiles sont interdites, nous prendrons des sanctions en fonction du caractère\n répétitif de cette infraction.` },
-        { name: '**REGLE N° 6 :**', value: `Tout contenu à caractère violent (sang , cadavre ...) ou sexuel est interdit  , il peut y'avoir des mineurs dans ce serveur.` },
-        { name: '**REGLE N° 7 :**', value: `Si une personne est en désaccord avec vous ne la forcez pas. ` }
+        { name: '**REGLE N° 1 :**', value: `\`Le respect et la rigueur sont  obligatoires` },
+        { name: '**REGLE N° 2 :**', value: `\`Les propos racistes, homophobes ou discriminations qui auraient pour but d'insulter ou blesser quelqu'un,\n entraineront un ban définitif de ce serveur.\`` },
+        { name: '**REGLE N° 3 :**', value: `\`Les publicités sont interdits sauf dans le salon ${client.channels.cache.get("1019716736209145936")}, pour tout partenariat il vous faudra ouvrir un ticket pour ainsi\n en parler avec l'administration.\`` },
+        { name: '**REGLE N° 4 :**', value: `\`Toute forme de harcèlement entrainera un ban définitif.\`` },
+        { name: '**REGLE N° 5 :**', value: `\`Toutes identifications inutiles sont interdites, nous prendrons des sanctions en fonction du caractère\n répétitif de cette infraction.\`` },
+        { name: '**REGLE N° 6 :**', value: `\`Tout contenu à caractère violent (sang , cadavre ...) ou sexuel est interdit  , il peut y'avoir des mineurs dans ce serveur.\`` },
+        { name: '**REGLE N° 7 :**', value: `\`Si une personne est en désaccord avec vous ne la forcez pas. \`` }
     )
     .setFooter({ text: 'Bienvenue sur le serveur' })
     .setTimestamp()
